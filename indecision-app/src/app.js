88c3ac30c776232fa291ecfe42e923b77bc70a89
Mehdi <elwafi.courrier99@gmail.com>
2021-07-21 19:25:50 +0100

    added src folder

---
+++ indecision-app/src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running');
+
+// JSX Javascript XML
+var template = React.createElement("p", null, "this is JSX from app.js");
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
