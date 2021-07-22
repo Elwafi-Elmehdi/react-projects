0a58a26a181d5aca5bd804f4bbb16e6527fe921c
Mehdi <elwafi.courrier99@gmail.com>
2021-07-22 17:52:24 +0100

    Explored jsx syntaxe

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,39 +1,39 @@
 console.log('App.js is running');
 
-var app = {
- title: "Workout",
- subTitle:"Run 3 km in 2 days",
- options:['one','two']
+const app = {
+  title: "Workout",
+  subTitle: "Run 3 km in 2 days",
+  options: ['one', 'two']
 }
 
 // JSX Javascript XML
-var template = (
- <div>
-  <h1>{app.title}</h1>
-  {app.subTitle && <p>{app.subTitle}</p> }
-  {app.options.length > 0 ? <p>Here is your options</p> : <p>No options</p>}
-  {app.options && <ol><li>{app.options[0]}</li><li>{app.options[1]}</li></ol>}
- </div>
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subTitle && <p>{app.subTitle}</p>}
+    {app.options.length > 0 ? <p>Here is your options</p> : <p>No options</p>}
+    {app.options && <ol><li>{app.options[0]}</li><li>{app.options[1]}</li></ol>}
+  </div>
 );
-var user = {
- name:"ELWAFI",
- age:23,
- location:"Marrakech"
+const user = {
+  name: "ELWAFI",
+  age: 23,
+  location: "Marrakech"
 }
 const getLocation = (location) => {
- if(location)
-  return <p>Location: {location}</p>
+  if (location)
+    return <p>Location: {location}</p>
 }
 
-var templateTwo = (
- <div>
-  <h1>{user.name ? user.name : 'Anonymous' }</h1>
-  { (user.age && user.age > 18) && <p>Age : {user.age}</p>}
-  <p>{getLocation(user.location)}</p>
- </div>
+const templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age > 18) && <p>Age : {user.age}</p>}
+    <p>{getLocation(user.location)}</p>
+  </div>
 );
 
-var appRoot = document.getElementById('app')
+const appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
 // ReactDOM.render(templateTwo,appRoot);
