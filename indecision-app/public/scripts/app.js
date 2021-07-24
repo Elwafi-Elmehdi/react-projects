94b9355ed5260a106224c442a87222fd983f1435
Mehdi <elwafi.courrier99@gmail.com>
2021-07-24 20:23:42 +0100

    Exploring es6 classes

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,97 +1,10 @@
 "use strict";
 
-console.log('App.js is running');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var app = {
-  title: "Workout",
-  subTitle: "Run 3 km in 2 days",
-  options: []
-};
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    renderApp();
-  }
+var Person = function Person() {
+  _classCallCheck(this, Person);
 };
 
-var removeAll = function removeAll() {
-  app.options = [];
-  renderApp();
-};
-
-var appRoot = document.getElementById('app');
-
-var guessOption = function guessOption() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
-};
-
-var renderApp = function renderApp() {
-  var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      app.title
-    ),
-    app.subTitle && React.createElement(
-      "p",
-      null,
-      app.subTitle
-    ),
-    app.options.length > 0 ? React.createElement(
-      "p",
-      null,
-      "Here is your options"
-    ) : React.createElement(
-      "p",
-      null,
-      "No options"
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length
-    ),
-    React.createElement(
-      "button",
-      { disabled: app.options.length === 0, onClick: guessOption },
-      "What Should I do ?"
-    ),
-    React.createElement(
-      "button",
-      { onClick: removeAll },
-      "removeAll"
-    ),
-    React.createElement(
-      "ol",
-      null,
-      app.options.map(function (elem) {
-        return React.createElement(
-          "li",
-          { key: elem },
-          elem
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add Option"
-      )
-    )
-  );
-
-  ReactDOM.render(template, appRoot);
-};
-
-renderApp();
+var me = new Person();
+console.log(me);
