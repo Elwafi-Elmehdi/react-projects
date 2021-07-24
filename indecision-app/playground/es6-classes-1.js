f0955b69a7a9c6a5ed9c50d5e9f77704e37e6fd5
Mehdi <elwafi.courrier99@gmail.com>
2021-07-24 20:31:13 +0100

    Exploring es6 classes part 1

--- indecision-app/playground/es6-classes-1.js
+++ indecision-app/playground/es6-classes-1.js
@@ -1,6 +1,11 @@
 class Person {
-
+ constructor(name = 'name') {
+  this.name = name
+ }
 }
 
-const me = new Person()
+const me = new Person('Elmehdi ELWAFI')
 console.log(me);
+
+const other = new Person()
+console.log(other);
--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,17 @@
-"use strict";
+'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Person = function Person() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'name';
+
   _classCallCheck(this, Person);
+
+  this.name = name;
 };
 
-var me = new Person();
+var me = new Person('Elmehdi ELWAFI');
 console.log(me);
+
+var other = new Person();
+console.log(other);
