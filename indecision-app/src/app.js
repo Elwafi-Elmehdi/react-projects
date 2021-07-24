27efa3ce83a7a1e9f1aa1f03d71d8b0a2687df45
Mehdi <elwafi.courrier99@gmail.com>
2021-07-24 01:52:03 +0100

    added render

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,24 +3,42 @@ console.log('App.js is running');
 const app = {
   title: "Workout",
   subTitle: "Run 3 km in 2 days",
-  options: ['one', 'two']
+  options: []
 }
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    {app.options.length > 0 ? <p>Here is your options</p> : <p>No options</p>}
-    <ol>
-      <li>{app.options[0]}</li>
-      <li>{app.options[1]}</li>
-    </ol>
-    <form>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault()
+  const option = e.target.elements.option.value
+  if (option) {
+    app.options.push(option)
+    e.target.elements.option.value = ''
+    renderApp()
+  }
+}
+
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      {app.options.length > 0 ? <p>Here is your options</p> : <p>No options</p>}
+      <p>{app.options.length}</p>
+      <button onClick={ }>removeAll</button>
+      <ol>
+        <li>{app.options[0]}</li>
+        <li>{app.options[1]}</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  )
+
+  ReactDOM.render(template, appRoot)
+}
+
+renderApp()
