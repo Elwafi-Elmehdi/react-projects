7b40a9de0ed2859897d13455339ae7878459f6a4
Mehdi <elwafi.courrier99@gmail.com>
2021-07-26 23:15:20 +0100

    Added state to counter app

--- indecision-app/playground/counter-jsx.js
+++ indecision-app/playground/counter-jsx.js
@@ -4,10 +4,15 @@ class CounterApp extends React.Component {
     this.handleAddOne = this.handleAddOne.bind(this)
     this.handleMinusOne = this.handleMinusOne.bind(this)
     this.handleReset = this.handleReset.bind(this)
+    this.state = {
+      count: 0
+    }
   }
 
   handleAddOne() {
-    console.log('handleAddOne');
+    this.setState(() => {
+      return {}
+    })
   }
   handleMinusOne() {
     console.log('handleMinusOne');
@@ -18,7 +23,7 @@ class CounterApp extends React.Component {
   render() {
     return (
       <div>
-        <h1>Count :</h1>
+        <h1>Count : {this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -19,13 +19,18 @@ var CounterApp = function (_React$Component) {
     _this.handleAddOne = _this.handleAddOne.bind(_this);
     _this.handleMinusOne = _this.handleMinusOne.bind(_this);
     _this.handleReset = _this.handleReset.bind(_this);
+    _this.state = {
+      count: 0
+    };
     return _this;
   }
 
   _createClass(CounterApp, [{
     key: 'handleAddOne',
     value: function handleAddOne() {
-      console.log('handleAddOne');
+      this.setState(function () {
+        return {};
+      });
     }
   }, {
     key: 'handleMinusOne',
@@ -46,7 +51,8 @@ var CounterApp = function (_React$Component) {
         React.createElement(
           'h1',
           null,
-          'Count :'
+          'Count : ',
+          this.state.count
         ),
         React.createElement(
           'button',
