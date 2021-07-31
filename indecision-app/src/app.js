4bfcd9a634f81a1ee17c05f72a0d04e965a16670
Mehdi <elwafi.courrier99@gmail.com>
2021-07-31 18:53:26 +0100

    Added and imported npm module

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,3 @@
-// import substract, { carre, add } from './utils.js'
-// console.log('sasas');
-// console.log(carre(4));
-// console.log(add(4, 100));
-// console.log(substract(1, 2));
+import validator from 'validator'
 
-import isSenior, { canDrink, isAdult } from './person.js'
-
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(15));
+console.log(validator.isEmail('mehdi@ksaks.com'));
