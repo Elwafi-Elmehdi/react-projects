ce86b299550e329db16730bf138f579a5cd5034d
Mehdi <elwafi.courrier99@gmail.com>
2021-08-02 17:30:58 +0100

    refactored all stateless cmps

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-const Option = (props) => {
- return (
-  <div>
-   <p key={props.optionText}>{props.optionText}</p>
-   <button
-    onClick={(e) => props.deleteOne(props.optionText)}
-   >remove
-   </button>
-  </div>
- )
-}
-export default Option
+const Option = (props) => (
+	<div>
+		<p key={props.optionText}>{props.optionText}</p>
+		<button onClick={(e) => props.deleteOne(props.optionText)}>
+			remove
+		</button>
+	</div>
+);
+
+export default Option;
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Option from './Option'
+import Option from "./Option";
 
-const Options = (props) => {
- return (
-  <div>
-   <button onClick={props.deleteAll}>Remove All Options</button>
-   {
-    props.options.map(elem =>
-     <Option
-      key={elem}
-      optionText={elem}
-      deleteOne={props.deleteOne}
-     />
-    )
-   }
-  </div>
- )
-}
-export default Options
+const Options = (props) => (
+	<div>
+		<button onClick={props.deleteAll}>Remove All Options</button>
+		{props.options.map((elem) => (
+			<Option key={elem} optionText={elem} deleteOne={props.deleteOne} />
+		))}
+	</div>
+);
+
+export default Options;
