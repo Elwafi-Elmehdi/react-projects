208180b4a24ef1b0fa2a62a3db19a9bff777099b
Mehdi <elwafi.courrier99@gmail.com>
2021-08-04 23:31:46 +0100

    Applied container on header

--- indecision-app/src/components/Header.js
+++ indecision-app/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 const Header = (props) => (
 	<div className="header">
-		<h1 className="header__title">{props.title}</h1>
-		<h2 className="header__sub-title">{props.subTitle}</h2>
+		<div className="container">
+			<h1 className="header__title">{props.title}</h1>
+			<h2 className="header__sub-title">{props.subTitle}</h2>
+		</div>
 	</div>
 );
 
