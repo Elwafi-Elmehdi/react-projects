99483a9cc05d8aea1ead750dcab613c54a886fd1
Mehdi <elwafi.courrier99@gmail.com>
2021-08-05 21:46:01 +0100

    Added styles to action btn

--- indecision-app/src/components/Action.js
+++ indecision-app/src/components/Action.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Action = (props) => (
 	<div>
-		<button onClick={props.handleRandom} disabled={!props.hasOptions}>
+		<button
+			className="big-button"
+			onClick={props.handleRandom}
+			disabled={!props.hasOptions}
+		>
 			what should I do?
 		</button>
 	</div>
