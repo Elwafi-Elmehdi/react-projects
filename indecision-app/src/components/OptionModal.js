4e84027c0ba4a8bc27a769cceb8427bba543715f
Mehdi <elwafi.courrier99@gmail.com>
2021-08-06 22:38:08 +0100

    Added some twiks in css

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -9,9 +9,13 @@ const OptionModal = (props) => (
 		closeTimeoutMS={200}
 		className="modal"
 	>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.handleSelectedOption}>Close</button>
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && (
+			<p className="modal__body">{props.selectedOption}</p>
+		)}
+		<button className="modal__body" onClick={props.handleSelectedOption}>
+			Close
+		</button>
 	</Modal>
 );
 
