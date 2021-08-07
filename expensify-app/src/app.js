82d1d1d91f5f6734ce0e179748cb9bc182edf5c5
Mehdi <elwafi.courrier99@gmail.com>
2021-08-07 19:29:22 +0100

    React boilerplate app with webpack and babel

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndicisionApp from "./components/IndecisionApp";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<IndicisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>React Boilerplate App</p>, document.getElementById("app"));
