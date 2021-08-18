c9e0e5804f7b37b34bf8783e5a70fd2c1c897407
Mehdi <elwafi.courrier99@gmail.com>
2021-08-19 00:02:36 +0100

    Added routes to dummy cmps

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -4,4 +4,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>React Boilerplate App</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => <div>this is from my dashboard cmp</div>;
+const AddExpensePage = () => <div>this is from my Add Expense cmp</div>;
+
+const routes = (
+	<BrowserRouter>
+		<div>
+			<Route path="/" component={ExpenseDashboardPage} />
+			<Route path="/create" component={AddExpensePage} />
+		</div>
+	</BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById("app"));
