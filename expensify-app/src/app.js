1fba7933837459bcec743b04ef842cccf036d369
Mehdi <elwafi.courrier99@gmail.com>
2021-08-19 01:59:44 +0100

    Added Switch from react router

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -8,15 +8,17 @@ const ExpenseDashboardPage = () => <div>this is from my dashboard cmp</div>;
 const AddExpensePage = () => <div>this is from my Add Expense cmp</div>;
 const HelpPage = () => <div>this is from Help page</div>;
 const EditExpensePage = () => <div>this is from Edit Expense cmp</div>;
+const NotFoundPage = () => <div>this is from Not Found page</div>;
 
 const routes = (
 	<BrowserRouter>
-		<div>
+		<Switch>
 			<Route path="/" exact={true} component={ExpenseDashboardPage} />
 			<Route path="/create" component={AddExpensePage} />
 			<Route path="/edit" component={EditExpensePage} />
 			<Route path="/help" component={HelpPage} />
-		</div>
+			<Route component={NotFoundPage} />
+		</Switch>
 	</BrowserRouter>
 );
 ReactDOM.render(routes, document.getElementById("app"));
