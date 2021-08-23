f6f1ed5b498d7abddfe8062d3105f37345de7878
Mehdi <elwafi.courrier99@gmail.com>
2021-08-23 20:42:53 +0100

    Exploring Redux part 6

--- expensify-app/playground/redux101.js
+++ expensify-app/playground/redux101.js
@@ -11,23 +11,33 @@ const store = createStore((state = initState, action) => {
 	switch (action.type) {
 		case "INCREMENT":
 			return { count: state.count + 1 };
-			break;
 		case "DECREMENT":
 			return { count: state.count - 1 };
+		case "RESET":
+			return { count: 0 };
 		default:
 			return state;
-			break;
 	}
 });
 
 console.log(store.getState());
 
 // Actions to be defined are increment, decrement, reset
-
-// Lets create an action increment
 // store.dispatch declare the action obj to the store
+
+// Lets create an increment action
+store.dispatch({
+	type: "INCREMENT",
+});
+
+// Lets create a decrement action
 store.dispatch({
 	type: "DECREMENT",
+});
+
+// Lets create a reset action
+store.dispatch({
+	type: "RESET",
 });
 
 console.log(store.getState());
