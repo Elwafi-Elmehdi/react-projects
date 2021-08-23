7d5268bb354dd50c9ec2e8fd3e8e8353216c2bf0
Mehdi <elwafi.courrier99@gmail.com>
2021-08-23 20:32:22 +0100

    Exploring Redux part 3

--- expensify-app/playground/redux101.js
+++ expensify-app/playground/redux101.js
@@ -1,11 +1,22 @@
 import { createStore } from "redux";
 
+// Initial State object to assign to state in create store
 const initState = {
 	count: 0,
 };
 
-const store = createStore((state = initState) => {
+// Global singleton that stores the state
+const store = createStore((state = initState, action) => {
+	// cheking for the type of the action
 	return state;
+});
+
+// Actions to be defined are increment, decrement, reset
+
+// Lets create an action increment
+// store.dispatch declare the action obj to the store
+store.dispatch({
+	type: "INCREMENT",
 });
 
 console.log(store.getState());
