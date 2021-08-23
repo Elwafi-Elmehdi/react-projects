447c5a3283699713fdf1a06d18763a7ea19530ba
Mehdi <elwafi.courrier99@gmail.com>
2021-08-23 23:10:20 +0100

    Exploring array/obj destructring

--- expensify-app/playground/destructering.js
+++ expensify-app/playground/destructering.js
@@ -1,1 +1,11 @@
-console.log("destructering");
+const address = ["Quartier foo 457", "Marrakech", "Morocco"];
+
+const [street, city, country] = address;
+
+console.log(`${street} ${city} ${country}`);
+
+// const [street, , country] = address; // skipping city
+
+// const [, city, country] = address; // skipping street
+
+// onst [, city="Rabat", country] = address; // defaulting city var to Rabat
