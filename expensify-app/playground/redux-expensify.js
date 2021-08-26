62c6f8272d0f50911083738b7f6206176bfd6241
Mehdi <elwafi.courrier99@gmail.com>
2021-08-26 23:14:57 +0100

    Added case in filters reducer

--- expensify-app/playground/redux-expensify.js
+++ expensify-app/playground/redux-expensify.js
@@ -103,6 +103,11 @@ const filtersInitState = {
 
 const filtersReducer = (state = filtersInitState, action) => {
 	switch (action.type) {
+		case "SET_TEXT_FILTER":
+			return {
+				...state,
+				text: action.text,
+			};
 		default:
 			return state;
 	}
