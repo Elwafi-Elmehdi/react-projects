1d1f1fdcb24ae865100510edb3f0afd365e8e390
Mehdi <elwafi.courrier99@gmail.com>
2021-08-28 18:58:21 +0100

    Added sorting on date and amount

--- expensify-app/playground/redux-expensify.js
+++ expensify-app/playground/redux-expensify.js
@@ -148,16 +148,25 @@ const showVisibleExpenses = (
 	expenses,
 	{ text, sortBy, startDate, endDate }
 ) => {
-	return expenses.filter((expense) => {
-		const title = expense.title.toLowerCase();
-		const textMatch = title.includes(text.toLowerCase());
-		const startDateMatch =
-			typeof startDate !== "number" || expense.createdAt >= startDate;
-		const endDateMatch =
-			typeof endDate !== "number" || expense.createdAt <= endDate;
+	return expenses
+		.filter((expense) => {
+			const title = expense.title.toLowerCase();
+			const textMatch = title.includes(text.toLowerCase());
+			const startDateMatch =
+				typeof startDate !== "number" || expense.createdAt >= startDate;
+			const endDateMatch =
+				typeof endDate !== "number" || expense.createdAt <= endDate;
 
-		return textMatch && startDateMatch && endDateMatch;
-	});
+			return textMatch && startDateMatch && endDateMatch;
+		})
+		.sort((a, b) => {
+			if (sortBy === "date") {
+				return a.createdAt < b.createdAt ? 1 : -1;
+			}
+			if (sortBy === "amount") {
+				return a.amount < b.amount ? 1 : -1;
+			}
+		});
 };
 
 // Every time the state changed the callbackc will be excuted
@@ -182,10 +191,10 @@ const expenseThree = store.dispatch(
 // // Lets edit an expense
 // store.dispatch(editExpense(expenseOne.expense.id, { amount: 4560 }));
 // // Lets Add Text filter
-store.dispatch(addTextFilter("PS"));
+// store.dispatch(addTextFilter("PS"));
 // store.dispatch(addTextFilter());
 // // Lets sort by amount
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // // Lets sort by date
 // store.dispatch(sortByDate());
 
