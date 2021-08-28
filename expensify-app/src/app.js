79bbcc56085674e6ac7e264e636bbe44fa6d9b01
Mehdi <elwafi.courrier99@gmail.com>
2021-08-28 19:57:23 +0100

    Tested redux in expensify app

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import showExpenses from "./selectors/showExpenses";
+import { addExpense } from "./actions/expenses";
+import { addTextFilter } from "./actions/filters";
+
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
 const store = configureStore();
-console.log(store.getState());
+
+store.dispatch(addExpense({ title: "Rent bill", amount: 2500 }));
+store.dispatch(addExpense({ title: "Car bill", amount: 3000 }));
+store.dispatch(addTextFilter("car"));
+
+const state = store.getState();
+console.log(showExpenses(state.expenses, state.filters));
+
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
