832acc49caeaa4b6ff838f4d0b5320e713e40d9b
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 18:17:52 +0100

    Added airbnb react dates

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -6,18 +6,17 @@ import showExpenses from "./selectors/showExpenses";
 
 import AppRouter from "./routers/AppRouter";
 import { addExpense } from "./actions/expenses";
-import { addTextFilter } from "./actions/filters";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
+const state = store.getState();
 
 store.dispatch(addExpense({ title: "Rent bill", amount: 2500 }));
 store.dispatch(addExpense({ title: "Car bill", amount: 3000 }));
 store.dispatch(addExpense({ title: "Gas bill", amount: 200 }));
 
-const state = store.getState();
 console.log(showExpenses(state.expenses, state.filters));
 
 const jsx = (
