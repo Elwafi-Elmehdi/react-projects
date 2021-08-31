9f668014cdeabab846ff4131b16c5095c5e0d10f
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 16:49:54 +0100

    Updated Expense form cmp

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 export default class ExpenseForm extends React.Component {
+	state = {
+		description: "",
+		amount: 0,
+		note: "",
+	};
+	onChangeDescription = (e) => {
+		const description = e.target.value;
+		this.setState((state) => ({
+			description,
+		}));
+	};
 	render() {
-		return <div>
-            This is form Expense Form</div>;
+		return (
+			<div>
+				<form>
+					<input
+						type="text"
+						value={this.state.description}
+						placeholder="Title"
+						autoFocus
+						onChange={this.onChangeDescription}
+					/>
+					<input
+						type="number"
+						value={this.state.amount}
+						placeholder="Amount"
+					/>
+					<textarea
+						placeholder="Note"
+						value={this.state.note}
+					></textarea>
+				</form>
+			</div>
+		);
 	}
 }
