5f9c63c62d70bfee63fb2fc31fb029f472e65a11
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 10:51:40 +0100

    Added remove expense action

--- expensify-app/src/components/ExpensesListItem.js
+++ expensify-app/src/components/ExpensesListItem.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpensesListItem = ({ expense }) => (
+const ExpensesListItem = (props) => (
 	<div>
-		<h3>{expense.title}</h3>
-		<h3>{expense.amount}</h3>
-		<h3>{expense.createdAt}</h3>
-		<button>Remove</button>
+		<h3>{props.expense.title}</h3>
+		<h3>{props.expense.createdAt}</h3>
+		<h3>{props.expense.amount}</h3>
+		<button
+			onSubmit={(e) => {
+				props.dispatch(removeExpense(props.expense.id));
+			}}
+		>
+			Remove
+		</button>
 	</div>
 );
 
-export default ExpensesListItem;
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(ExpensesListItem);
