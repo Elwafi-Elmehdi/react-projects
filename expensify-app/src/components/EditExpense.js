fb81d25ca8bed27e076610f31ff83d8995a47d16
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 22:25:36 +0100

    Completed edit function and navigation to home page

--- expensify-app/src/components/EditExpense.js
+++ expensify-app/src/components/EditExpense.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import { connect } from "react-redux";
+import { editExpense } from "../actions/expenses";
 const EditExpensePage = (props) => {
 	return (
 		<div>
 			<ExpenseForm
 				onSubmit={(expense) => {
-					console.log(props);
+					props.dispatch(editExpense(props.match.params.id, expense));
+					props.history.push("/");
 				}}
 				expense={props.expenses.find(
 					(expense) => expense.id === props.match.params.id
