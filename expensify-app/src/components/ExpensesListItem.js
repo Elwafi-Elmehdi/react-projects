5454e2442e84021adc3f2cc0ee804a704a59a13a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 20:22:58 +0100

    Added link for edit expense

--- expensify-app/src/components/ExpensesListItem.js
+++ expensify-app/src/components/ExpensesListItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { removeExpense } from "../actions/expenses";
+import { Link } from "react-router-dom";
 
 const ExpensesListItem = ({ dispatch, expense }) => (
 	<div>
 		<h3>{expense.title}</h3>
 		<h3>{expense.createdAt}</h3>
 		<h3>{expense.amount}</h3>
+		<Link to={`/edit/${expense.id}`}>Edit</Link>
 		<button
 			onClick={() => {
 				dispatch(removeExpense({ id: expense.id }));
