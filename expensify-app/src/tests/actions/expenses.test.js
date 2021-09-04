eb912ba83366613211b93f9dedc566cf33277e30
Mehdi <elwafi.courrier99@gmail.com>
2021-09-04 20:43:24 +0100

    Added test case for edit expense action

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -1,9 +1,21 @@
 import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
 
+// Test Case For Remove Expense Action
 test("Should set up remove expense action object", () => {
 	const action = removeExpense({ id: "123abc" });
 	expect(action).toEqual({
 		type: "REMOVE_EXPENSE",
 		id: "123abc",
+	});
+});
+
+// Test Case For Edit Expense Action
+
+test("Should set up edit expense action object", () => {
+	const action = editExpense("456def", { amount: 1000 });
+	expect(action).toEqual({
+		type: "EDIT_EXPENSE",
+		id: "456def",
+		updates: { amount: 1000 },
 	});
 });
