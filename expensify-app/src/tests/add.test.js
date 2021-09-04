c6bd6da6c250a93f37a5bee6cbc33658324c72be
Mehdi <elwafi.courrier99@gmail.com>
2021-09-04 20:23:31 +0100

    Exploring Jest 2

--- expensify-app/src/tests/add.test.js
+++ expensify-app/src/tests/add.test.js
@@ -1,8 +1,17 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name = "Ali") => `Hello ${name}`;
 
 test("Add 2 numbers", () => {
 	const result = add(3, 7);
-	if (result !== 10) {
-		throw new Error(`Expected 10 got ${result}`);
-	}
+	expect(result).toBe(10);
+});
+
+test("Greet by name", () => {
+	const result = generateGreeting("Mehdi");
+	expect(result).toBe("Hello Mehdi");
+});
+
+test("Greeting for no name", () => {
+	const result = generateGreeting();
+	expect(result).toBe("Hello Ali");
 });
