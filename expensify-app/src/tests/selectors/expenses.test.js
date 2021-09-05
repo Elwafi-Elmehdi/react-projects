033ebc2e16b5b680e21610a8be81c8d2f5db5080
Mehdi <elwafi.courrier99@gmail.com>
2021-09-05 19:46:37 +0100

    Added test case for select by text

--- expensify-app/src/tests/selectors/expenses.test.js
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -1,5 +1,51 @@
 import selectExpenses from "../../selectors/showExpenses";
 
-const expenses = [];
+const expenses = [
+	{
+		id: 1,
+		title: "Hego",
+		amount: 45.0,
+		note: "Testing is awesome",
+		createdAt: 7854,
+	},
+	{
+		id: 2,
+		title: "Car Payment",
+		amount: 3000,
+		note: "",
+		createdAt: -7854,
+	},
+	{
+		id: 3,
+		amount: 700,
+		title: "Real-state taxes",
+		note: "",
+		createdAt: 45,
+	},
+];
 
-test("", () => {});
+test("Shoud filter by text value", () => {
+	const filters = {
+		text: "a",
+		sortBy: "date",
+		startDate: 0,
+		endDate: 0,
+	};
+	const action = selectExpenses(expenses, filters);
+	expect(action).toEqual([
+		{
+			id: 3,
+			amount: 700,
+			title: "Real-state taxes",
+			note: "",
+			createdAt: 45,
+		},
+		{
+			id: 2,
+			title: "Car Payment",
+			amount: 3000,
+			note: "",
+			createdAt: -7854,
+		},
+	]);
+});
