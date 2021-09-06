7f0b6db21c35a3ec19301a1ba8a2b5c8f290f567
Mehdi <elwafi.courrier99@gmail.com>
2021-09-06 18:37:11 +0100

    Added test case for remove expense no id

--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -6,9 +6,26 @@ const expenses = [
 	{ id: 77, title: "Hehi 8" },
 ];
 
-// Test Case : Remove expense
+// Test Case : Remove expense with id
 test("Should remove an expense by id", () => {
 	const action = { type: "REMOVE_EXPENSE", id: 78 };
 	const state = expenseReducer(expenses, action);
 	expect(state).toEqual([expenses[1]]);
+});
+
+// Test Case : Remove expense without existing id
+test("Should remove non exsiting expense", () => {
+	const action = { type: "REMOVE_EXPENSE", id: 87 };
+	const state = expenseReducer(expenses, action);
+	expect(state).toEqual(expenses);
+});
+
+// Test Case : Add Expense default
+test("Should add an expense default ", () => {
+	const expense = {
+		title: "Mehdi",
+	};
+	const action = { type: "ADD_EXPENSE", expense };
+	const state = expenseReducer(expenses, action);
+	expect(state).toEqual([...expenses, expense]);
 });
