6010a296f50f03e1122168a9c9b325dac105ae2d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-06 18:05:38 +0100

    Added filter test case for sort by amount  reducer

--- expensify-app/src/tests/reducers/filters.test.js
+++ expensify-app/src/tests/reducers/filters.test.js
@@ -13,6 +13,13 @@ test("Should generate default values for filter", () => {
 	});
 });
 
+// Test Case : Action sort by amount
+test("Should set sortBy to amount", () => {
+	const action = { type: "SORT_BY_AMOUNT" };
+	const state = filersReducer(undefined, action);
+	expect(state.sortBy).toBe("amount");
+});
+
 // Test Case : Action sort by date
 test("Should set sortBy to date", () => {
 	const action = { type: "SORT_BY_DATE" };
