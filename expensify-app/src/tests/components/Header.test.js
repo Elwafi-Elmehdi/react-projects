04efa2bc87aedc6903796ee62e2eb05ae077919a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-07 23:09:03 +0100

    Added test case for header output

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,1 +1,10 @@
 // React Test Renderer npm module used for rendering cmps in files js
+import React from "react";
+import ReactShallowRenderer from "react-test-renderer/shallow";
+import Header from "../../components/Header";
+
+test("Should render header component", () => {
+	const renderer = new ReactShallowRenderer();
+	renderer.render(<Header />);
+	console.log(renderer.getRenderOutput());
+});
