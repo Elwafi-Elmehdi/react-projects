917fe897c45f354af4d0aeea912a6a42c6fae776
Mehdi <elwafi.courrier99@gmail.com>
2021-09-10 00:41:58 +0100

    Updated Header test suite

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -2,13 +2,12 @@
 // import ReactShallowRenderer from "react-test-renderer/shallow";
 import React from "react";
 import Header from "../../components/Header";
-import toJson from "enzyme-to-json";
 
 import { shallow } from "enzyme";
 
 test("Should render header component", () => {
 	const wrapper = shallow(<Header />);
-	expect(toJson(wrapper)).toMatchSnapshot();
+	expect(wrapper).toMatchSnapshot();
 	// expect(wrapper.find("h1").text()).toBe("Expensify");
 	// const renderer = new ReactShallowRenderer();
 	// renderer.render(<Header />);
