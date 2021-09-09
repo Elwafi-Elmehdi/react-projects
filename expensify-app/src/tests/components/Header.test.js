d00898c0a234cc63b891b969e39861f72395c056
Mehdi <elwafi.courrier99@gmail.com>
2021-09-10 00:39:19 +0100

    Exploring enzyme testing

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,11 +1,17 @@
 // React Test Renderer npm module used for rendering cmps in files js
+// import ReactShallowRenderer from "react-test-renderer/shallow";
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
 import Header from "../../components/Header";
+import toJson from "enzyme-to-json";
+
+import { shallow } from "enzyme";
 
 test("Should render header component", () => {
-	const renderer = new ReactShallowRenderer();
-	renderer.render(<Header />);
-	const result = renderer.getRenderOutput();
-	expect(result).toMatchSnapshot();
+	const wrapper = shallow(<Header />);
+	expect(toJson(wrapper)).toMatchSnapshot();
+	// expect(wrapper.find("h1").text()).toBe("Expensify");
+	// const renderer = new ReactShallowRenderer();
+	// renderer.render(<Header />);
+	// const result = renderer.getRenderOutput();
+	// expect(result).toMatchSnapshot();
 });
