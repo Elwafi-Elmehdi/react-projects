c41f60a0050d79eafec6402a0653d686c8841554
Mehdi <elwafi.courrier99@gmail.com>
2021-09-10 17:10:36 +0100

    Added test case for expense list item

--- expensify-app/src/tests/components/ExpensesListItem.test.js
+++ expensify-app/src/tests/components/ExpensesListItem.test.js
@@ -1,2 +1,9 @@
 import React from "react";
 import ExpensesListItem from "../../components/ExpensesListItem";
+import { shallow } from "enzyme";
+import { expenses } from "../selectors/expenses.test";
+
+test("Should render expenselist item with props", () => {
+	const wrapper = shallow(<ExpensesListItem expense={expenses[1]} />);
+	expect(wrapper).toMatchSnapshot();
+});
