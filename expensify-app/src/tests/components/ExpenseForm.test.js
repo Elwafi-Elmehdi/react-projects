2e749150632af5eb36acf92f3d501736040d48c1
Mehdi <elwafi.courrier99@gmail.com>
2021-09-11 19:45:34 +0100

    Added test case snapshot for expense form wwith props

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { shallow } from "enzyme";
 import ExpenseForm from "../../components/ExpenseForm";
+import { expenses } from "../selectors/expenses.test";
 
 test("Should render expense form correctly", () => {
 	const wrapper = shallow(<ExpenseForm />);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test("Should render expense form with expense prop", () => {
+	const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
 	expect(wrapper).toMatchSnapshot();
 });
