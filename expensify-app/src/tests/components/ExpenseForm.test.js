f8358b1f8a1f28d72443726e8e28621366d6c07a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-12 17:07:48 +0100

    Added test case for user interaction

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,12 @@ test("Should render expense form with expense prop", () => {
 	const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
 	expect(wrapper).toMatchSnapshot();
 });
+
+test("Should render error msg",() => {
+	const wrapper = shallow(<ExpenseForm />)
+	expect(wrapper).toMatchSnapshot();
+	wrapper.find('form').simulate('submit',{
+		preventDefault() {}
+	})
+	expect(wrapper.state('error').length).toBeGreaterThan(0)
+})
