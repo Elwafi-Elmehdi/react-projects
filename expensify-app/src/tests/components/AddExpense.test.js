77666d163926574391ddac791c1fbfb3be2b62b0
Mehdi <elwafi.courrier99@gmail.com>
2021-09-14 20:50:33 +0100

    Updated Add Expense page snapshot

--- expensify-app/src/tests/components/AddExpense.test.js
+++ expensify-app/src/tests/components/AddExpense.test.js
@@ -2,9 +2,9 @@ import React from "react";
 import {shallow} from "enzyme";
 import {AddExpensePage} from "../../components/AddExpense";
 
-test("Should render AddExpensePage correclty",()=>{
+test("Should render AddExpensePage correctly",()=>{
     const onSubmit = jest.fn()
     const history = {push: jest.fn()}
-    const wrapper = shallow(<AddExpensePage/>)
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
     expect(wrapper).toMatchSnapshot()
 })
