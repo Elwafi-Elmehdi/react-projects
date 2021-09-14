d72550a22240bd97861e7ed8d7ef4a899df239ca
Mehdi <elwafi.courrier99@gmail.com>
2021-09-14 21:00:07 +0100

    Updated test onsubmit handler

--- expensify-app/src/tests/components/AddExpense.test.js
+++ expensify-app/src/tests/components/AddExpense.test.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {shallow} from "enzyme";
 import {AddExpensePage} from "../../components/AddExpense";
+import {expenses} from "../selectors/expenses.test";
 
 test("Should render AddExpensePage correctly",()=>{
     const onSubmit = jest.fn()
     const history = {push: jest.fn()}
     const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
     expect(wrapper).toMatchSnapshot()
+})
+
+test("Should render onSubmit",() => {
+    const onSubmit = jest.fn()
+    const history = {push: jest.fn()}
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0])
+    expect(history.push).toHaveBeenCalled()
 })
