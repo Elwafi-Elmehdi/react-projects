dc8fdcdce54ffc1955d405195965271ffef35a6d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-24 20:08:59 +0100

    Updated webpack config file

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -21,13 +21,26 @@ module.exports = (env) => {
 				{
 					test: /\.s?css$/,
 					use: CSSExtract.extract({
-						use: ["css-loader", "sass-loader"],
+						use: [
+							{
+								loader: "css-loader",
+								options: {
+									sourceMap: true,
+								},
+							},
+							{
+								loader: "sass-loader",
+								options: {
+									sourceMap: true,
+								},
+							},
+						],
 					}),
 				},
 			],
 		},
 		plugins: [CSSExtract],
-		devtool: isProd ? "source-map" : "eval-cheap-module-source-map",
+		devtool: isProd ? "source-map" : "inline-source-map",
 		devServer: {
 			contentBase: path.join(__dirname, "public"),
 			historyApiFallback: true,
