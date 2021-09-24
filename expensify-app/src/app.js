03c2600e4e42bad7cd1bdfb25c7ea28a138b358d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-24 18:03:42 +0100

    Expensify removed dummy data from app.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -10,12 +10,6 @@ import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(addExpense({ title: "Rent bill", amount: 2500 }));
-store.dispatch(addExpense({ title: "Car bill", amount: 3000 }));
-store.dispatch(addExpense({ title: "Gas bill", amount: 200 }));
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
