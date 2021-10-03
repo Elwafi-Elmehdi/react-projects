6a30f80ee5f023ac11b94e5cc7b54cbca0101b10
Mehdi <elwafi.courrier99@gmail.com>
2021-10-03 18:09:57 +0100

    Updated server.js

--- expensify-app/server/server.js
+++ expensify-app/server/server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static())
+const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "/public");
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+	console.log("Server is up");
+});
